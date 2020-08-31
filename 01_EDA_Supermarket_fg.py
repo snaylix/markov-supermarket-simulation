7c4b157fc8ca81ee45a9b4a2bd5fe568cb1c7e46
snay <feligges@gmx.de>
2020-08-31 15:58:41 +0200

    switched EDA to Jupyter Notebook

--- 01_EDA_Supermarket_fg.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('08_Supermarket_Analysis/_RES/monday.csv', sep=';')
-df.shape
-df.head()
-df.tail()
-# Calculate the total number of customers in each section
-df['location'].value_counts()
-# Calculate the total number of customers in each section over time
