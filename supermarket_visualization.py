6a32fb24f1f096bf3df7ac4917dda3c4651cdb1f
snay <feligges@gmx.de>
2020-09-03 21:23:45 +0200

    randomly moving customer // changes color when created

--- supermarket_visualization.py
+++ supermarket_visualization.py
@@ -1,6 +1,7 @@
 import cv2
 import pandas as pd
 from random import choices, randint
+import time
 
 LOCATION = {'entrance': (700, 850),
             'fruits': (400, 850),
@@ -9,66 +10,55 @@ LOCATION = {'entrance': (700, 850),
             'drinks': (400, 150),
             'checkout': (700, 400)}
 
-COLOR = {1: (212, 38, 167),
-         2: (222, 30, 78),
-         3: (169, 74, 37),
-         4: (222, 165, 30),
-         5: (178, 212, 29),
-         6: (205, 114, 255)}
-
-##### TEMPORARY FOR INPUTTING TRANSITION MATRIX.
+# TEMPORARY FOR INPUTTING TRANSITION MATRIX.
 # TO DO -- change into class?
 
 MATRIX = pd.read_csv('supermarket_transitions.csv', index_col=0).T.to_dict()
 
 
-class Color:
-
-    def __init__(self):
-        self.red = randint(0, 255)
-        self.green = randint(0, 255)
-        self.blue = randint(0, 255)
-
-
 class Location:
-    history = []
 
-    def __init__(self):
-        ...
+    def __init__(self, y=700, x=850, current_location='entrance'):
+        self.x = x
+        self.y = y
+        self.current_location = current_location
 
-    def next_loc(self, current_loc, probabilities):
+    def next_location(self):
         '''
         MCMC simulation of customer behaviour
         current_loc: where is customer at start
         probabilities:
         '''
-        probabilities = MATRIX[self.current_loc]
-        while self.current_loc != 'checkout':
-            self.current_loc = choices(self.current_loc, probabilities)[0]
-            history.append(self.current_loc)
+        history = []
+        probabilities = MATRIX[self.current_location]
+        while self.current_location != 'checkout':
+            self.current_location = choices(self.current_location, probabilities)[0]
+            history.append(self.current_location)
         return history
 
 
 class Customer:
 
-    def __init__(self, color, x=850, y=700, name='entrance'):
-        self.x = x
-        self.y = y
-        self.color = color
-        self.name = name
+    def __init__(self, y=700, x=850, current_location='entrance'):
+        self.y = LOCATION[current_location][0]
+        self.x = LOCATION[current_location][1]
+        self.color = (randint(0, 255), randint(0, 255), randint(0, 255))
+        self.current_location = current_location
 
     def draw(self, frame):
         frame[self.y:self.y+20, self.x:self.x+20] = self.color
 
     def move(self):
         """moves depending on next location"""
-        ...
+        next = choices(['fruits', 'spices', 'dairy', 'drinks', 'checkout'])
+        self.y = LOCATION[next[0]][0]
+        self.x = LOCATION[next[0]][1]
 
     def __repr__(self):
         return f"customer at {self.x}/{self.y}"
 
 
-c1 = Customer(Location(), Color())
+c1 = Customer()
 
 market = cv2.imread('market.png')
 
@@ -79,6 +69,8 @@ while True:
     c1.draw(frame)
 
     cv2.imshow('frame', frame)
+
+    time.sleep(1)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
